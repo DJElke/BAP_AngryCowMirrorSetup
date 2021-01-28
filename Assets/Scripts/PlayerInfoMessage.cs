a173f99f2f5fcc8649555c71e35e2909f8369874
DJElke <elkedejonckere@gmail.com>
2021-01-29 00:23:50 +0100

    Different player spawn part 1 - different prefabs

---
+++ Assets/Scripts/PlayerInfoMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public enum PlayerClass
+{
+    Cow,
+    Farmer
+}
+public struct PlayerInfoMessage : NetworkMessage
+{
+    public PlayerClass playerClass;
+    public string description;
+    public bool isHost;
+}
